fdef77ebafef13b4de5a3657c54aefd51a12a9eb
Georg Orlov <orlov@altek.online>
2024-12-24 13:19:44 +0500

    Создал окошко с надписью "Hello world!", которая появляется, по нажатию на 2ю кнопку

--- source/Application/Commands/StartupCommand_2.cs
+++ source/Application/Commands/StartupCommand_2.cs
@@ -2,6 +2,10 @@
 using Nice3point.Revit.Toolkit.External;
 using Module_2.ViewModels;
 using Module_2.Views;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.UI;
+using System.Windows.Controls;
+using System.Windows;
 
 namespace Application.Commands
 {
@@ -14,9 +18,41 @@ namespace Application.Commands
     {
         public override void Execute()
         {
-            var viewModel = new Module_2ViewModel();
-            var view = new Module_2View(viewModel);
-            view.ShowDialog();
+            var stackPanel = new StackPanel();
+
+            // Создаем окно и добавляем в него стековую панель
+            var messageBox = new Window
+            {
+                Title = "Information",
+                Content = stackPanel,
+                SizeToContent = SizeToContent.WidthAndHeight,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen
+            };
+
+            // Создаем текстовый блок
+            var textBlock = new TextBlock
+            {
+                Text = "Hello world",
+                TextAlignment = TextAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Margin = new Thickness(20)
+            };
+
+            // Создаем кнопку "Ок"
+            var okButton = new Button
+            {
+                Content = "Ok",
+                Width = 75,
+                Margin = new Thickness(10)
+            };
+            okButton.Click += (sender, args) => messageBox.Close();
+
+            // Создаем стековую панель и добавляем в нее текстовый блок и кнопку            
+            stackPanel.Children.Add(textBlock);
+            stackPanel.Children.Add(okButton);
+
+            // Показываем окно
+            messageBox.ShowDialog();
         }
     }
 }
