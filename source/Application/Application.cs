72fca01d343c9c66a6686862e95c6864d7d67854
Georg Orlov <orlov@altek.online>
2024-12-24 15:33:03 +0500

    Изменил название панели

--- source/Application/Application.cs
+++ source/Application/Application.cs
@@ -1,4 +1,7 @@
-﻿using Application.Commands;
+﻿using System.Reflection;
+using System.Windows.Media.Imaging;
+using Application.Commands;
+using Autodesk.Revit.UI;
 using Nice3point.Revit.Toolkit.External;
 
 namespace Application
@@ -16,11 +19,9 @@ namespace Application
 
         private void CreateRibbon()
         {
-            var panel = Application.CreatePanel("Commands", "Altek Orlov");
+            var panel = Application.CreatePanel("Test Task 1", "Altek Orlov");
 
             panel.AddPushButton<StartupCommand>("Тестовая кнопка")
-                //.SetLargeImage("/Application;component/Resources/Icons/ico_new_1_32.png");
-                //.SetLargeImage("/Application;component/Resources/Icons/ico_new_2_32.png");
                 .SetLargeImage("/Application;component/Resources/Icons/ico_test_2.png");
 
             panel.AddPushButton<StartupCommand_2>("Генерация круга из стен")
