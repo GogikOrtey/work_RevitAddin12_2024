7a9091eecc137648972c83ca50f95c535286c972
Georg Orlov <orlov@altek.online>
2024-12-25 15:06:58 +0500

    I wrote a handler for incorrect input in the radius field

--- source/Module_2/Views/Module_2View.xaml.cs
+++ source/Module_2/Views/Module_2View.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 using Autodesk.Revit.UI;
 using Module_2.ViewModels;
 
@@ -18,17 +19,38 @@ namespace Module_2.Views
             this.Close();
         }
 
+        // Кнопка подтверждения генерации стен
         private void Button_Click_AcceptGenerateWall(object sender, System.Windows.RoutedEventArgs e)
         {
+            string inputRadius = TB_inputRadius.Text;
+            double radius;
+            bool isNumber = double.TryParse(inputRadius, out radius);
+
+            if (!isNumber)
+            {
+                // Значение не является числом, установите переменную в false
+                // Дополнительно, вы можете уведомить пользователя, что введено некорректное значение
+                MessageBox.Show("Введите числовое значение для радиуса.", "Ошибка ввода", MessageBoxButton.OK, MessageBoxImage.Warning);
+                ErrorTextBlock_forIncorrectIngectRadius.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                // Значение является числом, можете использовать переменную radius
+
+                TaskDialog.Show("Info", "Значение корректно, и = " + radius);
+            }
+
+
+
             //TaskDialog.Show("Заголовок окна", "Текст внутри окна");
 
             // Проверить корректность ввода в числовом поле радиуса
-                // Если ввод некорректен - то показать текст, что ввод неверный
+            // Если ввод некорректен - то показать текст, что ввод неверный
         }
 
         private void Button_Click_Test1(object sender, System.Windows.RoutedEventArgs e)
         {
-            ErrorTextBlock_forIncorrectIngectRadius.Visibility = System.Windows.Visibility.Visible;
+            //ErrorTextBlock_forIncorrectIngectRadius.Visibility = System.Windows.Visibility.Visible;
         }
     }
 }
