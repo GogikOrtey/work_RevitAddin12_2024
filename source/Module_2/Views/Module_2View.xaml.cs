423a6b9888a07f4791d2dc652032d5f2fd8f6f2c
Georg Orlov <orlov@altek.online>
2024-12-25 14:52:10 +0500

    Added the button click processing code

--- source/Module_2/Views/Module_2View.xaml.cs
+++ source/Module_2/Views/Module_2View.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Module_2.ViewModels;
+﻿using System.Windows.Controls;
+using Autodesk.Revit.UI;
+using Module_2.ViewModels;
 
 namespace Module_2.Views
 {
@@ -8,6 +10,25 @@ namespace Module_2.Views
         {
             DataContext = viewModel;
             InitializeComponent();
+        }
+
+        private void Button_Click_CloseThisWindow(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Закрытие окна
+            this.Close();
+        }
+
+        private void Button_Click_AcceptGenerateWall(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //TaskDialog.Show("Заголовок окна", "Текст внутри окна");
+
+            // Проверить корректность ввода в числовом поле радиуса
+                // Если ввод некорректен - то показать текст, что ввод неверный
+        }
+
+        private void Button_Click_Test1(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ErrorTextBlock_forIncorrectIngectRadius.Visibility = System.Windows.Visibility.Visible;
         }
     }
 }
