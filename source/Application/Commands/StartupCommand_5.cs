1772c467dc4b6ae8594e3543ee1686e5464b1695
Georg Orlov <orlov@altek.online>
2025-01-09 12:14:20 +0500

    The information window about the version display is working correctly

--- source/Application/Commands/StartupCommand_5.cs
+++ source/Application/Commands/StartupCommand_5.cs
@@ -1,20 +1,8 @@
-﻿using Autodesk.Revit.Attributes;
-using Nice3point.Revit.Toolkit.External;
-using Module_2.ViewModels;
-using Module_2.Views;
-using Autodesk.Revit.UI.Selection;
-using Autodesk.Revit.UI;
-using System.Windows.Controls;
-using System.Windows;
-using System.Windows.Media;
-using Autodesk.Revit.DB;
-using CommunityToolkit.Mvvm.DependencyInjection;
+﻿#define REVIT_2024
+#define REVIT_2025
 
-#if REVIT_24
-#define REVIT_24
-#elif REVIT_25
-#define REVIT_25
-#endif
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
 
 namespace Application.Commands
 {
@@ -27,12 +15,12 @@ namespace Application.Commands
         // Код, который выполняется по нажатию кнопки 4
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            #if REVIT_24
+            #if REVIT_2024
                 TaskDialog.Show("Информация", "Это версия Revit 24");
-            #elif REVIT_25
+            #elif REVIT_2025
                 TaskDialog.Show("Информация", "Это версия Revit 25");
             #else
-                TaskDialog.Show("Информация", "Неизвестная версия Revit");
+                TaskDialog.Show("Информация", "Версия Revit неизвестна");
             #endif
 
             return Result.Succeeded;
